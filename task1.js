07010867caed429354ae5f36f7fd5d18adb4c386
codingveenit <116004094+codingveenit@users.noreply.github.com>
2023-11-04 15:21:25 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,27 @@
+function addBlog() {
+  // Retrieve input values
+  var title = document.getElementById("title").value;
+  var image = document.getElementById("image").value;
+  var content = document.getElementById("content").value;
+  var video = document.getElementById("video").value;
+
+
+  // Create HTML structure for the blog post
+  var blogPost = document.createElement("div");
+  blogPost.className = "blog-post";
+  blogPost.innerHTML = `
+      <h3>${title}</h3>
+      <img src="${image}" alt="Blog Image">
+      <p>${content}</p>
+    `;
+
+  // Add the blog post to the preview section
+  var previewSection = document.getElementById("preview");
+  previewSection.appendChild(blogPost);
+
+  // Clear input fields after adding the blog
+  document.getElementById("title").value = "";
+  document.getElementById("image").value = "";
+  document.getElementById("video").value = "";
+  document.getElementById("content").value = "";
+}
